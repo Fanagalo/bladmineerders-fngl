5351f06cf9b7d1964ca5dd23ba87ab59e1b1248d
Jaap Wiering <github.com@fanagalo.nl>
2022-02-03 17:18:01 +0100

    glossary layout letterbox

--- archive-glossary.php
+++ archive-glossary.php
@@ -15,52 +15,47 @@ get_header();
     <div class="entry-content">
 
         <header class="page-header">
-
-		<h1><?php printf( __( '
-			[:nl]Terminologie[:]
-			[:en]Glossary[:]
-		') ); ?></h1>
-
+			<h1><?php printf( __( '
+				[:nl]Terminologie[:]
+				[:en]Glossary[:]
+			') ); ?></h1>
         </header>
 
 		<?php
 			if ( have_posts() ) :
-			echo '<dl class="glossary">';
+				echo '<dl class="glossary">';
 
-			// sort titles alphabetically
-			$found_posts = array();
-		
-			foreach ( $wp_query->posts as $k=>$p ) {
-				$found_posts[ apply_filters('the_title', $p->post_title) ] = $p;
-			}
-			ksort($found_posts, SORT_NATURAL | SORT_FLAG_CASE);
-			$i=0;
+				// sort titles alphabetically
+				$found_posts = array();
 
-			foreach ($found_posts as $k=>$p) {
-				$wp_query->posts[$i++] = $p;
-			}
+				foreach ( $wp_query->posts as $k=>$p ) {
+					$found_posts[ apply_filters('the_title', $p->post_title) ] = $p;
+				}
 
-			while ( have_posts() ) :
-				the_post();
+				ksort($found_posts, SORT_NATURAL | SORT_FLAG_CASE);
+				$i=0;
 
-				the_title( '<dt>', '</dt>' );
-				echo '<dd>';
-				the_content( '', '' );
-				echo '</dd>';
+				foreach ($found_posts as $k=>$p) {
+					$wp_query->posts[$i++] = $p;
+				}
 
-			endwhile;
+				while ( have_posts() ) :
+					the_post();
+					the_title( '<dt>', '</dt>' );
+					echo '<dd>';
+					the_content( '', '' );
+					echo '</dd>';
+				endwhile;
 
-			echo '</dl>';
-		endif; ?>
+				wp_reset_postdata();
+
+				echo '</dl>';
+			endif; ?>
 
     </div><!-- .entry-content -->
-
 
 </main>
 <div class="main-area-full"></div>
 
-
-
 <?php
-
 get_footer();
