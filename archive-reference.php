9f10872b70778a7189e146adac27ab7b7fe91e4a
Jaap Wiering <github.com@fanagalo.nl>
2022-02-04 15:14:02 +0100

    find panel in reference

--- archive-reference.php
+++ archive-reference.php
@@ -20,6 +20,33 @@ get_header();
 				[:en]References[:]
 			') ); ?></h1>
         </header>
+
+        <?php printf( __( '
+				<div class="panel light-bg">
+
+            [:nl]
+			
+                <h2>Sneller zoeken met browser functionaliteit</h2>
+
+                <p>Deze literatuurlijst bestaat uit één webpagina met alle, duizenden bronverwijzingen. 
+                Een auteur is het makkelijkst te vinden door de browser (Chrome, Safari, Firefox, Edge) het zoekwerk te laten doen. 
+                Gebruik de toetscombinatie <strong>ctrl + F</strong> (Windows) of <strong>cmd + F</strong> (macOS). 
+                Deze methode is sneller en vollediger dan scrollen door de pagina.</p>
+            [:en]
+
+                <h2>Faster search with browser functionality</h2>
+
+                <p>This list of references consists of one webpage that contains all thousands of source references. 
+                The fastest way to find an author is to let the browser (Chrome, Safari, Firefox, Edge) search for you. 
+                Use the keyboard shortcut <strong>ctrl + F</strong> (Windows) of <strong>cmd + F</strong> (macOS).
+                This method is faster and more comprehensive than scrolling the page.</p>
+            [:]
+                </div>
+		') ); ?>
+
+
+
+
         <a name="top"></a>
 
         <?php
