b54f13b6d3082cc19fc9cf08cf98f223ae1b95ed
Jaap Wiering <github.com@fanagalo.nl>
2023-11-15 16:07:18 +0100

    add host-genus-galler template

---
+++ host-genus-galler.php
@@ -0,0 +1,35 @@
+<?php
+/**
+Template Name: Host - Genus - Galler
+ *
+ * Custom template used for Genera of Hosts
+ * Displays text from the content field: mainly a dichotomous table to the Parasite Species
+ * Displays a hierarchical tree of lower taxa
+ * Shows the modification date
+ * THIS IS A TEMPORARY SOLUTION in the process of making Hosts into a CPT
+ *
+ * @package bladmineerders-fngl
+ * @since version 0.75
+ */
+
+get_header();
+?>
+
+<div class="main-area-full"></div>
+<main id="main" class="main-area">
+
+	<?php
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'template-parts/content', 'page' );
+		endwhile; // End of the loop.
+	?>
+
+
+</main>
+<div class="main-area-full"></div>
+
+<?php
+get_template_part( 'inc/modification-date' );
+get_sidebar();
+get_footer();
