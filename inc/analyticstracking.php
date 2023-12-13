5d95fb7b9621a9a5b4206a115a85e54bc3bb4326
Jaap Wiering <github.com@fanagalo.nl>
2023-12-13 16:08:19 +0100

    GA disabled

--- inc/analyticstracking.php
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-17809614-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-17809614-2', { 'anonymize_ip': true });
-</script>
