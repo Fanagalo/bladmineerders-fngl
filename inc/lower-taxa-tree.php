c49f5598f7e69b2e109a0677adb231ad632852ff
Jaap Wiering <github.com@fanagalo.nl>
2025-08-19 15:04:56 +0200

    changed SQL

--- inc/lower-taxa-tree.php
+++ inc/lower-taxa-tree.php
@@ -14,13 +14,14 @@ function fngl_template_lower_taxa_tree($depth,$templates) {
     global $wpdb;
 
     $sql = "
-        SELECT ID,post_title,post_parent 
-        FROM $wpdb->posts,$wpdb->postmeta 
-        WHERE ID = post_id
-        AND meta_value IN(".implode(', ', array_fill(0, count($templates), '%s')).")
-        ORDER BY menu_order,post_title ASC
-    ";
-    // AND post_status = 'publish' could be added, but doubles page generation time
+        SELECT p.ID, p.post_title, p.post_parent, p.menu_order
+        FROM $wpdb->posts p
+        INNER JOIN $wpdb->postmeta pm ON p.ID = pm.post_id
+        WHERE pm.meta_value IN(" . implode(', ', array_fill(0, count($templates), '%s')) . ")
+        ORDER BY p.menu_order, p.post_title ASC
+        ";
+
+    // AND p.post_status = 'publish' could be added, but doubles page generation time
 
     $query = call_user_func_array(array($wpdb, 'prepare'), array_merge(array($sql), $templates));
     $results = $wpdb->get_results($query);
